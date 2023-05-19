bcf6154b7ace84125ee8e93673ad4c38969e6395
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-05-19 12:51:27 -0300

    Update app-root with the camera stream hook

--- src/app-root/app-root.ts
+++ src/app-root/app-root.ts
@@ -1,10 +1,26 @@
 import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, query } from "lit/decorators.js";
+import { cameraStream } from "../hooks/camera-stream";
 
 @customElement("app-root")
 export class AppRoot extends LitElement {
+  @query(".pepitos-video") video!: HTMLVideoElement;
+
+  async firstUpdated() {
+    await cameraStream({
+      video: this.video,
+      constraints: {
+        audio: false,
+        video: { width: 1280, height: 720 },
+      },
+    });
+  }
+
   render() {
-    return html` <h1>Pepitos!</h1> `;
+    return html`
+      <h1>Pepitos!</h1>
+      <video class="pepitos-video"></video>
+    `;
   }
 }
 
