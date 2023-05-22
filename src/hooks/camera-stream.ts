5cbc80e3db40a9cb1202417fea42f2cb1202c51a
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-05-22 17:31:57 -0300

    Update cameraStream with return type

--- src/hooks/camera-stream.ts
+++ src/hooks/camera-stream.ts
@@ -3,12 +3,16 @@ interface ICameraStream {
   constraints: MediaStreamConstraints;
 }
 
-export const cameraStream = async ({ video, constraints }: ICameraStream) => {
+export const cameraStream = async ({
+  video,
+  constraints,
+}: ICameraStream): Promise<MediaStream | undefined> => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     video.srcObject = stream;
     video.style.transform = "scaleX(-1)";
     video.onloadedmetadata = () => video.play();
+    return stream;
   } catch (error) {
     console.error("Error accessing video stream", error);
   }
